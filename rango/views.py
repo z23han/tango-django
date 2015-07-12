62f7231120623d6a76d77800be4390ca7332d1ed
Daniel Han <zxhan2012@gmail.com>
2015-07-12 13:56:48 -0400

    template of template files

--- rango/views.py
+++ rango/views.py
@@ -23,7 +23,8 @@ def index(request):
 	# return HttpResponse("Rango says hey there world!<br/><a href='/rango/about'>About</a>")
 
 def about(request):
-	return HttpResponse("Rango says here is the about page<br/><a href='/rango'>Index</a>")
+	return render(request, 'rango/about.html', {})
+	# return HttpResponse("Rango says here is the about page<br/><a href='/rango'>Index</a>")
 
 
 def category(request, category_name_slug):
